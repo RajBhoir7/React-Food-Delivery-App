ff53e7cea5179e7e42ea8d549829e9fe665e6107
Raj bhoir <bhoirraj872@gmail.com>
2025-07-29 16:51:43 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement(
+//     'h1',
+//     {id:"heading",class:"main"},
+//     "Hello World From React...");
+
+
+
+const parent = React.createElement('div',{id:"parent"},[React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+]);
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
